941dac760a5a74569b9f146b351cc71253baa462
kdr253 <k.d.r.velagala@accenture.com>
2025-10-06 13:09:51 +0900

    Update client.ts

--- ui/src/api/client.ts
+++ ui/src/api/client.ts
@@ -2,7 +2,7 @@ import createClient from 'openapi-fetch';
 
 import type { paths } from './schema';
 
-export const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://127.0.0.1:8080';
+export const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 export const client = createClient<paths>({
   baseUrl,
   headers: {},
